e4b6799edb641642c745c3c170ff331a445c7bad
cwood576 <cwood576@yandex.ru>
2023-02-13 21:13:07 +0400

    исправил роутинг

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/api', require('./routes/index'));
+app.use('/', require('./routes/index'));
 
 app.use(errorLogger);
 
